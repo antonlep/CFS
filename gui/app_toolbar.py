51f46fb80d11b3b13b158cb3b413b40a921c8709
antonlep <antonleppa@gmail.com>
2025-12-12 20:00:05 +0200

    gui dialog

--- gui/app_toolbar.py
+++ gui/app_toolbar.py
@@ -37,9 +37,7 @@ class MainWindow(QMainWindow):
 
         toolbar.addSeparator()
 
-        button_action2 = QAction(
-            QIcon("assets/bug.png"), "Your &button2", self
-        )
+        button_action2 = QAction(QIcon("assets/bug.png"), "Your &button2", self)
         button_action2.setStatusTip("This is your button2")
         button_action2.triggered.connect(self.toolbar_button_clicked)
         button_action2.setCheckable(True)
